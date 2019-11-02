f02e7a6e830a881a169847483671589d7793155c
Herman Starikov <hermanstarikov@gmail.com>
2019-11-02 12:01:36 -0400

    update screenshots

--- scripts/screenshots.js
+++ scripts/screenshots.js
@@ -4,8 +4,8 @@ run({
   destination: "build/screenshots",
   saveAs: "png",
   viewport: {
-    width: 400,
-    height: 400
+    width: 1200,
+    height: 628
   },
   puppeteerArgs: ["--no-sandbox", "--disable-setuid-sandbox"],
   puppeteerExecutablePath: "/opt/google/chrome/google-chrome"
