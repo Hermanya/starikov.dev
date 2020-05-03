f3efa658b08c796dfaa90c43547582b10d3068be
Herman Starikov <hermanstarikov@gmail.com>
2020-05-03 14:28:04 -0400

    Save

--- src/responsive-page/index.tsx
+++
@@ -1,5 +0,0 @@
-import ResponsiveReactApp from "./ResponsiveReactApp";
-import TabBar, { TabBarLink, TabBarLinkWithAnIcon } from "./TabBar";
-
-export default ResponsiveReactApp;
-export { TabBar, TabBarLink, TabBarLinkWithAnIcon };
---
+++ src/slots/Herman.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import About from "components/landing/About";
+export default () => {
+  return <About />;
+};
