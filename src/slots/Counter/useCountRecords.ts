2538689da3a43751fa31603ebbfbd3c67664127b
Herman Starikov <hermanstarikov@gmail.com>
2020-06-28 21:31:30 -0400

    Save

--- src/slots/Counter/useCountRecords.ts
+++ src/slots/Counter/useCountRecords.ts
@@ -1,9 +1,5 @@
 import createPersistedState from "use-persisted-state";
 
-if (localStorage.counter) {
-  localStorage.PushUps = localStorage.counter;
-  delete localStorage.counter;
-}
 export const useCountRecords = createPersistedState("PushUps");
 export type CountRecord = {
   timestamp: number;
