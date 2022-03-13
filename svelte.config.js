2f45e33c927acfdfa00a8543238bf5fade0d00b0
Herman Starikov <herman@starikov.dev>
2022-03-12 21:35:46 -0500

    Clean up

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "dist",
+      assets: "dist",
+    }),
 
     // Override http methods in the Todo forms
     methodOverride: {
