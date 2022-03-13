d0136dabae6e1c42f39c82958d94e5c51ad7ec1e
Herman Starikov <herman@starikov.dev>
2022-03-12 20:27:30 -0500

    Fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "hosting": {
-        "S3AndCloudFront": {
-            "Region": "string",
-            "HostingBucketName": "string",
-            "WebsiteURL": "string",
-            "S3BucketSecureURL": "string",
-            "CloudFrontDistributionID": "string",
-            "CloudFrontDomainName": "string",
-            "CloudFrontSecureURL": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
